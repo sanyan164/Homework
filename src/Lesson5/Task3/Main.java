e195099f1ec6036b75c48add109de343e0f689c4
ATozik <sanyan164@gmail.com>
2025-03-26 13:51:08 +0300

    Урок 5. Задание 3

--- src/Lesson5/Task3/Main.java
+++ src/Lesson5/Task3/Main.java
@@ -1,7 +1,37 @@
 package Lesson5.Task3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<String> fruits = new ArrayList<>() {{
+            add("apple");
+            add("apple");
+            add("apple");
+            add("banana");
+            add("banana");
+            add("cherry");
+        }};
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String fruit : fruits){
+            if(map.containsKey(fruit)){
+                map.put(fruit, map.get(fruit)+1);
+            }else{
+                map.put(fruit, 1);
+            }
+        }
+        for (Map.Entry<String, Integer> i : map.entrySet()) {
+            String end;
+            String key = i.getKey();
+            int value = i.getValue();
+            if (value%10==2||value%10==3||value%10==4){
+                end = " раза!";
+            }else {
+                end = " раз!";
+            }
+            System.out.println("Слово: '"+ key + "' встречается " + value+ end);
+        }
     }
 }
